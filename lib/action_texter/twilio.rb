d367cc20dd0dff31b07dddb957fa9ca532aefa8f
Daniel Magliola <dmagliola@crystalgears.com>
2015-01-07 12:38:46 +0000

    Non-semantic style changes.

--- lib/action_texter/twilio.rb
+++ lib/action_texter/twilio.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-# Copyright © 2012, 2013, Watu
+# Copyright © 2014, 2015, Watu
 
 require "bigdecimal"
 require "net/http"
@@ -17,12 +17,12 @@ class ActionTexter::TwilioResponse < ActionTexter::Response
   def process_response(raw)
     @original = JSON.parse(raw)
 
-    if @original['error_code'].blank? && @original['code'].blank?
+    if @original["error_code"].blank? && @original["code"].blank?
       @success = true
-      @reference = @original['sid']
-      @parts_count = @original['num_segments']
+      @reference = @original["sid"]
+      @parts_count = @original["num_segments"]
 
-      # The cost is nil because the message is, as of now, queued. To get the cost we need to GET @original['uri']
+      # The cost is nil because the message is, as of now, queued. To get the cost we need to GET @original["uri"]
       # for message details, but that assumes that the message is sent by that time.
       # The proper way, a callback, is way out of the scope of this gem.
       @cost = nil
@@ -31,10 +31,10 @@ class ActionTexter::TwilioResponse < ActionTexter::Response
 
       # Responses take two shapes. Either they have a status like "queued", and they have an "error_code" field
       #    (which I've never seen filled), or they return a status like "400", with a "code" and a "message"
-      if @original.has_key?('code')
-        @error_message = @original['message']
-      elsif @original.has_key?('error_message')
-        @error_message = @original['error_message']
+      if @original.has_key?("code")
+        @error_message = @original["message"]
+      elsif @original.has_key?("error_message")
+        @error_message = @original["error_message"]
       end
     end
   end
@@ -59,7 +59,7 @@ class ActionTexter::TwilioClient < ActionTexter::Client
     http.use_ssl = true
     request = Net::HTTP::Post.new("/2010-04-01/Accounts/#{@account_sid}/Messages.json")
     request.basic_auth(@account_sid, @auth_token)
-    request.set_form_data({'From' => message.from, 'To' => message.to, 'Body' => message.text})
+    request.set_form_data({"From" => message.from, "To" => message.to, "Body" => message.text})
     response = http.request(request)
 
     return ActionTexter::TwilioResponse.new(response.body)
